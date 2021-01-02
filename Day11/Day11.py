19082e2051769c7111718c904d110402a705ca3b
Stefan <stevetv90@gmail.com>
2021-01-02 16:30:18 +0100

    Day 11 done

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -1,21 +1,39 @@
 line = open("input.txt","r").readlines()[0]
-counter = 0
-lastchar = ""
-for _ in range(50):
-    output = ""
-    for c in line:
-        if lastchar == "":
-            lastchar = c
-            counter += 1
-        elif lastchar == c:
-            counter += 1
-        else:
-            output += str(counter)+lastchar
-            lastchar = c
-            counter = 1
-    output += str(counter)+lastchar
-    lastchar = ""
-    counter = 0
-    line = output
+def rec(string):
+    if ord(string[-1])+1 == 123:
+        return rec(string[0:-1])+chr(97)
+    else:
+        return string[0:-1]+chr(ord(string[-1])+1)
 
-print(len(output))
+def checker(string):
+    tests = False
+    pairs = 0
+    lastpair = -1
+    if "i" in string or "o" in string or "l" in string:
+        return True
+    else:
+        for i in range(len(string)-1):
+            if i < len(string)-2:
+                if ord(string[i]) == ord(string[i+1])-1 == ord(string[i+2])-2:
+                    tests = True
+            if string[i] == string[i+1]:
+                if lastpair == -1:
+                    pairs +=1
+                elif i == lastpair+1 and string[i] == string[lastpair+1]:
+                    continue
+                else:
+                    pairs +=1
+                lastpair = i
+    if tests == True and pairs >= 2:
+        return False
+    else:
+        return True
+
+
+while checker(line) == True:
+    line = rec(line)
+print(line)
+line = rec(line)
+while checker(line) == True:
+    line = rec(line)
+print(line)
